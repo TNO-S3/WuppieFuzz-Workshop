8722601ebb777c5a0c587da6e368d96ea2e7280c
Ringo Groenewegen <ringo.groenewegen@tno.nl>
2025-08-21 11:05:06 +0200

    patches

---
+++ apply_patch.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Exit on failure
+set -e
+
+usage() {
+    echo
+    echo "Usage: $0 <apply patch>"
+    echo
+    echo "Arguments:"
+    echo "  <apply patch>    Indicate whether to apply a patch or not. Options: \"true\", \"false\""
+    echo
+
+    exit
+}
+
+if [ $# -ne 1 ]; then
+    echo "Error: missing argument"
+    usage
+fi
+if [ "$1" == "true" ]; then
+    APPLY_PATCH="True"
+elif [ "$1" == "false" ]; then
+# patch -p1 < ../petstore.patch
+    APPLY_PATCH=""
+else 
+    echo "Error: invalid option selected"
+    usage
+fi
+
+
+wget https://github.com/swagger-api/swagger-petstore/archive/refs/tags/swagger-petstore-v2-1.0.6.tar.gz
+tar xf swagger-petstore-v2-1.0.6.tar.gz         # Extract the contents
+rm swagger-petstore-v2-1.0.6.tar.gz
+cd swagger-petstore-swagger-petstore-v2-1.0.6/  # Go into the extracted directory
+
+if [ -z $APPLY_PATCH ]; then
+    # Don't apply the patch
+    :
+else
+    # Apply the patch
+    patch -p1 < ../petstore.patch
+fi
+
+cd ..
+
+docker build -t petstore .
+CONTAINER_ID=$(docker run -itd -p 8080:8080 petstore)
+sleep 5
+
+# Test the output
+curl -X 'POST' \
+  'http://localhost:8080/api/pet' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "id": 0,
+  "category": {
+    "id": 0,
+    "name": "string"
+  },
+  "name": "doggie",
+  "photoUrls": [
+    "string"
+  ],
+  "tags": "",
+  "status": "available"
+}'
+
+docker kill $CONTAINER_ID
---
+++ create_patch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ diff -uNr swagger-petstore-swagger-petstore-v2-1.0.6/ patched/ > petstore.patch
